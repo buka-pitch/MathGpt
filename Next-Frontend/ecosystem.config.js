dde957cab965b7eaa16e0af8717dfd1dd5d2baba
buka <birukyonas331@gmail.com>
2023-09-07 04:00:23 +0300

    deploy

--- Next-Frontend/ecosystem.config.js
+++ Next-Frontend/ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       host: '54.160.112.79',
       ref: 'origin/master',
       repo: 'https://github.com/buka-pitch/MathGpt.git',
-      path: '/home/ubuntu/MathGpt',
+      path: '/home/ubuntu',
       'pre-deploy-local': '',
       'post-deploy': 'cd Next-Frontend && source ./.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
